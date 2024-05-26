04299fed3e1397a1caac41a81d24a3e10db12372
AdrianMartinT <128418441+AdrianMartinT@users.noreply.github.com>
2024-05-26 20:54:22 +0200

    Update inferencia_constraint.sh

--- inferencia_constraint.sh
+++ inferencia_constraint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python3 ~/coral/pycoral/examples/classify_image.py --model ~/coral/pycoral/test_data/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite --labels ~/coral/pycoral/test_data/inat_bird_labels.txt --input ~/coral/pycoral/test_data/parrot.jpg
+echo "============================================"
+date "+%H:%M:%S"
+python3 ~/coral/pycoral/examples/classify_image.py --model ~/coral/pycoral/test_data/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite --labels ~/coral/pycoral/test_data/inat_bird_labels.txt --input ~/coral>
+date "+%H:%M:%S"
+echo "============================================"
